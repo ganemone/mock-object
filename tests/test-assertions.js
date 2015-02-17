f5b4f9c5b8eab30870ab599cacc64783303e2ff2
Giancarlo Anemone <ganemone@gmail.com>
2015-02-17 15:07:43 -0500

    Add tests

---
+++ tests/test-assertions.js
@@ -0,0 +1,37 @@
+var assert = require('chai').assert;
+var makeMock = require('../lib/makeMock');
+
+var api = {
+  someFunction: function() {
+  },
+  prototype: {
+    anotherFunction: function() {
+    }
+  }
+}
+
+var mockAPI = makeMock(api);
+var newAPI = new mockAPI();
+
+describe('makeMock', function () {
+  beforeEach(function () {
+    mockAPI.reset();
+    newAPI.reset();
+  });
+  it('should mock object properties correctly', function () {
+    assert.equal(typeof mockAPI.someFunction, 'function');
+    assert.equal(typeof mockAPI.someFunction.assertCalledOnceWith, 'function');
+  });
+  it('should mock object prototype correctly', function () {
+    assert.equal(typeof newAPI.anotherFunction, 'function');
+    assert.equal(typeof newAPI.anotherFunction.assertCalledOnceWith, 'function');
+  });
+  it('should allow definition of return values', function () {
+    mockAPI.someFunction.returns(10);
+    assert.equal(mockAPI.someFunction(), 10);
+  });
+  it('should allow definition of return values on prototype functions', function () {
+    newAPI.anotherFunction.returns('some value');
+    assert.equal(newAPI.anotherFunction(), 'some value');
+  });
+});
